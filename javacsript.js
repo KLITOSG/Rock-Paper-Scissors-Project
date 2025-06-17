6283e4d4ee727281da62d0ee421116f22dcdd84f
KLITOSG <gaitanosklitos@gmail.com>
2025-06-17 15:13:55 +0300

    wrote the logic of the round and the pseudocode tested it

--- javacsript.js
+++ javacsript.js
@@ -4,10 +4,10 @@
      code that will return the computers choice 
      
 */
-let choice;
 
-function getComputerChoice (choice){
-   choice = Math.random()
+
+function getComputerChoice (){
+   let choice = Math.random()
 
     if(choice <= 0.30){
         return "Rock" ;
@@ -20,33 +20,86 @@ function getComputerChoice (choice){
     }
 } 
 
-getComputerChoice(choice);
+
 
 /* Funcion that takes the user's input
     create a prompt for useers input 
-    Code that returns the users input*/
+    Code that returns the users input
+*/
 
-let input;
 
-function getHumanChoice (input){
-    input = prompt('Lets play Rock-Paper-Scissors. Whenever you are ready write here your choice: ');
 
-    if(input == "Rock"){
-        return "Rock";
+function getHumanChoice (){
+    let input = prompt('Lets play Rock-Paper-Scissors. Whenever you are ready write here your choice: ');
 
-    } else if ( input == "Paper"){
-        return "Paper";
-
-    }else if ( input == "Scissors"){
-        return "Scissors" ;
-    }
+    return input ; 
 }
   
-console.log(getHumanChoice(input));
-getHumanChoice(input);
+
+
 
 /* declare variables for score: one variable for user and one for computer */
 
 let humanScore = 0;
 let computerScore = 0; 
 
+/* function that takes as arguments the computes and human choices  
+       code that makes the user input case insensitive
+       code that logs a string value representing the round winner, such as: “You lose! Paper beats Rock”.
+       code that increments the scores based in who wins the round
+*/
+
+function playRound(humanChoice,computerChoice){
+     if (humanChoice.localeCompare("Rock",undefined,{sensitivity:"base"})=== 0){
+        humanChoice = "Rock" ;
+
+     }else if(humanChoice.localeCompare("Paper",undefined,{sensitivity:"base"})=== 0){
+        humanChoice= "Paper" ;
+
+     }else if(humanChoice.localeCompare("Scissors",undefined,{sensitivity:"base"})=== 0){
+        humanChoice = "Scissors"
+     }
+
+         
+ if(humanChoice == "Rock" && computerChoice == "Rock"){
+        console.log ('Its a Tie !');
+        
+
+    } else if (humanChoice == "Rock" && computerChoice == "Paper"){
+        console.log("You lose ! Paper wraps the Rock");
+        computerScore ++ ;
+        
+
+    }else if(humanChoice == "Rock" && computerChoice == "Scissors"){
+       console.log('You win ! Rock brakes the Scissors');
+       humanScore ++ ;
+
+    }else if ( humanChoice == "Paper" && computerChoice =="Rock"){
+        console.log('You win ! Paper wraps the Rock');
+        humanScore ++ ;
+
+    }else if(humanChoice == "Paper" && computerChoice == "Paper"){
+        console.log('Its a tie !');
+
+    }else if(humanChoice == "Paper" && computerChoice == "Scissors"){
+        console.log('You lose! Scissors cut the Paper');
+        computerScore ++ ; 
+
+    }else if(humanChoice == "Scissors" && computerChoice == "Rock"){
+        console.log('You lose ! Rock breaks the Scissors') ;
+        computerScore ++ ;
+
+    }else if (humanChoice == "Scissors" && computerChoice == "Paper"){
+        console.log('You win ! Scissors cut the Paper');
+        humanScore ++ ;
+
+    }else if( humanChoice == "Scissors" && computerChoice == "Scissors"){
+        console.log('Its a tie !');
+    }
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice,computerChoice);
+console.log(humanScore,computerScore);
