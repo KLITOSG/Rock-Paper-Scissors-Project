c60c1a9252d4c4d0bbec83078dc7f790b007fc0d
KLITOSG <gaitanosklitos@gmail.com>
2025-06-17 15:50:06 +0300

    Write the last part of code that runs the game in 5 rounds---the end

--- javacsript.js
+++ javacsript.js
@@ -68,30 +68,36 @@ function playRound(humanChoice,computerChoice){
     } else if (humanChoice == "Rock" && computerChoice == "Paper"){
         console.log("You lose ! Paper wraps the Rock");
         computerScore ++ ;
-        
+        console.log(humanScore,computerScore);
 
     }else if(humanChoice == "Rock" && computerChoice == "Scissors"){
        console.log('You win ! Rock brakes the Scissors');
        humanScore ++ ;
+       console.log(humanScore,computerScore);
 
     }else if ( humanChoice == "Paper" && computerChoice =="Rock"){
         console.log('You win ! Paper wraps the Rock');
-        humanScore ++ ;
+        humanScore ++;
+        console.log(humanScore,computerScore);
 
     }else if(humanChoice == "Paper" && computerChoice == "Paper"){
         console.log('Its a tie !');
+        console.log(humanScore,computerScore);
 
     }else if(humanChoice == "Paper" && computerChoice == "Scissors"){
         console.log('You lose! Scissors cut the Paper');
         computerScore ++ ; 
+        console.log(humanScore,computerScore);
 
     }else if(humanChoice == "Scissors" && computerChoice == "Rock"){
         console.log('You lose ! Rock breaks the Scissors') ;
         computerScore ++ ;
+        console.log(humanScore,computerScore);
 
     }else if (humanChoice == "Scissors" && computerChoice == "Paper"){
         console.log('You win ! Scissors cut the Paper');
         humanScore ++ ;
+        console.log(humanScore,computerScore);
 
     }else if( humanChoice == "Scissors" && computerChoice == "Scissors"){
         console.log('Its a tie !');
@@ -101,5 +107,33 @@ function playRound(humanChoice,computerChoice){
 const humanChoice = getHumanChoice();
 const computerChoice = getComputerChoice();
 
-playRound(humanChoice,computerChoice);
-console.log(humanScore,computerScore);
+
+
+
+/*function that makes 5 rounds of this game
+    code that makes the game run 5 rounds*/
+
+function playGame(){
+   
+
+    for( rounds = 1; rounds <= 5; rounds ++){
+     const humanChoice = getHumanChoice();
+     const computerChoice = getComputerChoice();
+
+     console.log(`Round: ${rounds}`);
+     playRound(humanChoice,computerChoice);
+
+     console.log(`Score: You: ${humanScore} | Computer: ${computerScore}`);
+    }
+
+    if(humanScore<computerScore){
+        console.log("You lost !! :(");
+
+    }else if(humanScore>computerScore){
+        console.log('YOU WON !!! ;)');
+
+    }else{
+        console.log('Its a tie !!!')
+    }
+}
+playGame();
