0d1a52d8ba2fd4d2af608bb7fc30903f721689ca
KLITOSG <gaitanosklitos@gmail.com>
2025-06-17 12:11:18 +0300

    Write the function that defines the Computer choice ans test it

--- javacsript.js
+++ javacsript.js
@@ -1,6 +1,26 @@
-/**/ 
+/*Project Rock Papers Scissors: Javscript pseudocode
 
-function getComputerChoice(random){
-    random = Math.random()
-}
+  function to get the computer choice
+     code that will return the computers choice 
+     
+*/
+let choice;
+
+function getComputerChoice (choice){
+   choice = Math.random()
+
+    if(choice <= 0.30){
+        return "Rock" ;
+
+    } else if(choice <= 0.70){
+        return "Paper";
+
+    }else{
+        return "Scissors";
+
+    }
+    
+} 
+console.log(getComputerChoice(choice));
+
 
